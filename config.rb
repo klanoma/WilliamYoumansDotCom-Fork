7da311828c9a64f7755934e098c1c98572bcd1ea
William Youmans <will@williamyoumans.com>
2012-08-30 23:04:27 -0600

    updated font-awesome

--- config.rb
+++ config.rb
@@ -1,23 +1,9 @@
-# Require any additional compass plugins here.
-
-# Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "core/layout/css"
 sass_dir = "core/layout/sass"
 images_dir = "core/layout/images"
 javascripts_dir = "core/js"
-
 output_style = :compact
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
+relative_assets = true
 line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass core/layout/sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
