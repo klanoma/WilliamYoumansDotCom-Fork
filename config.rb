d9c84d004eade7d4a9612cfa5e909ad38284ceb6
William Youmans <will@williamyoumans.com>
2013-10-16 20:33:40 -0600

    updated font path

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ css_dir = "core/layout/css"
 sass_dir = "core/layout/sass"
 images_dir = "core/layout/images"
 javascripts_dir = "core/js"
-output_style = :compressed
+output_style = (environment == :production) ? :compressed : :expanded
 relative_assets = true
 line_comments = false
 preferred_syntax = :scss
